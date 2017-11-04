a4145aaed9f0c8c73938223ef0772d2eb0d91a31
Dan <dan,kirichok@gmail.com>
2017-11-04 06:55:11 -0400

    Added search bar and pop up to add outfit. Started changing color scheme to match app.

--- public/js/main_scripts.js
+++ public/js/main_scripts.js
@@ -4,10 +4,13 @@ function getRandomInt(min, max) {
 
 function randomizeOutfit(){
 	
+	//Fetches the total amount of items in each carousel of clothes
+	//Used in getting a random item from each category
 	var total_accesories = $("#accessories_slides").find(".item").length - 1;
 	var total_shirts = $("#shirts_slides").find(".item").length - 1;
 	var total_pants = $("#pants_slides").find(".item").length - 1;
 	
+	//Switches each carousel to a random item
 	$("#accessories_slides").carousel(getRandomInt(0, total_accesories));
 	$("#shirts_slides").carousel(getRandomInt(0, total_shirts));
 	$("#pants_slides").carousel(getRandomInt(0, total_pants));
